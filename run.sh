ce609881749617558a0acb888659e07ba9e17ffe
F.LuVx <yeyu0926@hotmail.com>
2021-12-07 18:23:43 +0800

    update

--- run.sh
+++ run.sh
@@ -1,27 +1,14 @@
+#!/bin/bash
+
 for i in \
         coding-dependencies.xml \
-        coding-java-parent.xml \
         coding-kotlin-dependencies.xml \
         coding-kotlin-enhancer/pom.xml \
-        coding-kotlin-parent.xml \
+        coding-parent-java.xml \
+        coding-parent-kotlin.xml \
         spring-boot-java.xml \
         spring-boot-kotlin.xml
 do
     mvn clean install -f $i
 done
 
-# 创建maven项目模板
-# cd sample
-# mvn archetype:create-from-project
-# cd target/generated-sources/archetype
-# mvn install
-
-# 使用模板创建项目
-mvn archetype:generate \
- -DgroupId=org.luvx \
- -DartifactId=usage-dataflow \
- -Dversion=1.0.1-SNAPSHOT \
- -Dpackage=org.luvx.app \
- -DarchetypeCatalog=local
-#  -DarchetypeArtifactId=luvx-sample-archetype \
-#  -DinteractiveMode=false
--- sample/src/main/java/org/luvx/app/App.java
+++
@@ -1,7 +0,0 @@
-package org.luvx.app;
-
-/**
- * @author Ren, Xie
- */
-public class App {
-}
