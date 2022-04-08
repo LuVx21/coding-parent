80d5ff8bf69b8179a99bd2eb08d93d650d57e63a
F.LuVx <yeyu0926@hotmail.com>
2022-04-08 16:56:18 +0800

    upduate

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 for i in \
-        coding-dependencies.xml \
-        coding-kotlin-dependencies.xml \
-        coding-kotlin-enhancer/pom.xml \
+        logging-parent/pom.xml \
+        logging-spring-parent/pom.xml \
+        coding-root.xml \
+        coding-root-kotlin.xml \
         coding-parent-java.xml \
         coding-parent-kotlin.xml \
         spring-boot-java.xml \
-        spring-boot-kotlin.xml
+        spring-boot-kotlin.xml \
+        coding-kotlin-enhancer/pom.xml
 do
     mvn clean install -f $i
 done
