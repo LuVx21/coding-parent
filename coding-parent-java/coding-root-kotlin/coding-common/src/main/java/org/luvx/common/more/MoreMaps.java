5cb356404470207dd921d126722f1b81ccbb6f2b
F.LuVx <yeyu0926@hotmail.com>
2022-06-08 20:45:41 +0800

    update

---
+++ coding-parent-java/coding-root-kotlin/coding-common/src/main/java/org/luvx/common/more/MoreMaps.java
@@ -0,0 +1,22 @@
+package org.luvx.common.more;
+
+import com.google.common.collect.ImmutableMap;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import static org.apache.commons.lang3.ArrayUtils.getLength;
+
+public class MoreMaps {
+    public static <K, V> Map<K, V> of(K[] ks, V... vs) {
+        ks = Arrays.stream(ks).distinct().toArray(l -> (K[]) new Object[l]);
+        if (getLength(ks) != getLength(vs)) {
+            throw new IllegalArgumentException("k存在重复或k-v数量不匹配");
+        }
+        ImmutableMap.Builder<K, V> builder = ImmutableMap.builder();
+        for (int i = 0; i < ks.length; i++) {
+            builder.put(ks[i], vs[i]);
+        }
+        return builder.build();
+    }
+}
