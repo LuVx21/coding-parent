c1a283e0c4346b216b9fcd1230a4d63221cd6510
F.LuVx <yeyu0926@hotmail.com>
2022-08-16 15:37:44 +0800

    update

--- coding-parent-java/coding-root-kotlin/coding-common/src/main/java/org/luvx/common/enums/EnumHasCode.java
+++ coding-parent-java/coding-root-kotlin/coding-common/src/main/java/org/luvx/common/enums/EnumHasCode.java
@@ -4,7 +4,6 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.Arrays;
 import java.util.Map;
-import java.util.Objects;
 
 import static java.util.Collections.emptyMap;
 import static java.util.function.Function.identity;
@@ -22,12 +21,13 @@ public interface EnumHasCode<T> {
 
     /**
      * 是否是: 实际用于业务标识的枚举
-     *
-     * @param unBizCode 不用于业务标识的枚举值, 如 UNKNOWN 对应的值
      */
-    static <E extends Enum<E> & EnumHasCode<T>, T> boolean isValidBizCode(final Class<E> enumClass, T code, T unBizCode) {
+    static <E extends Enum<E> & EnumHasCode<T>, T> boolean isValidBizCode(final Class<E> enumClass, T code) {
+        if (code == null) {
+            return false;
+        }
         E of = of(enumClass, code);
-        return of != null && !Objects.equals(of.getCode(), unBizCode);
+        return of != null && of.isValidBizCode();
     }
 
     static <E extends Enum<E> & EnumHasCode<T>, T> boolean isValidCode(final Class<E> enumClass, T code) {
@@ -36,6 +36,9 @@ public interface EnumHasCode<T> {
 
     @Nullable
     static <E extends Enum<E> & EnumHasCode<T>, T> E of(final Class<E> enumClass, T code) {
+        if (code == null) {
+            return null;
+        }
         return enumMap(enumClass).get(code);
     }
 
@@ -43,11 +46,16 @@ public interface EnumHasCode<T> {
      * 根据code找到对应枚举值, 不存在设置默认值
      */
     static <E extends Enum<E> & EnumHasCode<T>, T> E of(final Class<E> enumClass, T code, @Nonnull E defaultValue) {
+        if (code == null) {
+            return defaultValue;
+        }
         return enumMap(enumClass).getOrDefault(code, defaultValue);
     }
 
     /**
      * 带有 code 的枚举类的所有枚举值
+     * <p>
+     * 枚举值很多时可加工使用本地缓存
      *
      * @param enumClass 枚举类
      * @param <E>       枚举类型
