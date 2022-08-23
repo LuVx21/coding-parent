44bfa6287293e190062f7ace72dfb90caab70fdd
F.LuVx <yeyu0926@hotmail.com>
2022-08-23 20:29:27 +0800

    update

---
+++ coding-parent-boot-java/coding-parent-boot-kotlin/boot-web/src/main/java/org/luvx/boot/web/constraints/ByteMaxLength.java
@@ -0,0 +1,42 @@
+package org.luvx.boot.web.constraints;
+
+import org.luvx.boot.web.constraints.validator.ByteMaxLengthValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = {ByteMaxLengthValidator.class})
+@Target({
+        ElementType.METHOD, ElementType.FIELD,
+        ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR,
+        ElementType.PARAMETER, ElementType.TYPE_USE
+})
+@Retention(RetentionPolicy.RUNTIME)
+@Repeatable(ByteMaxLength.List.class)
+public @interface ByteMaxLength {
+    /**
+     * 最大字节长度
+     */
+    int max() default Integer.MAX_VALUE;
+
+    String charset() default "UTF-8";
+
+    Class<?>[] groups() default {};
+
+    String message() default "【${validatedValue}】的字节数已经超过最大值{max}";
+
+    Class<? extends Payload>[] payload() default {};
+
+    @Target({
+            ElementType.METHOD, ElementType.FIELD,
+            ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR,
+            ElementType.PARAMETER, ElementType.TYPE_USE
+    })
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    @interface List {
+        ByteMaxLength[] value();
+    }
+}
---
+++ coding-parent-boot-java/coding-parent-boot-kotlin/boot-web/src/main/java/org/luvx/boot/web/constraints/validator/ByteMaxLengthValidator.java
@@ -0,0 +1,48 @@
+package org.luvx.boot.web.constraints.validator;
+
+import org.luvx.boot.web.constraints.ByteMaxLength;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.nio.charset.Charset;
+
+public class ByteMaxLengthValidator implements ConstraintValidator<ByteMaxLength, String> {
+    private int     max;
+    private Charset charset;
+
+    @Override
+    public void initialize(ByteMaxLength constraintAnnotation) {
+        max = constraintAnnotation.max();
+        charset = Charset.forName(constraintAnnotation.charset());
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext constraintValidatorContext) {
+        if (value == null) {
+            return true;
+        }
+        boolean result = value.getBytes(charset).length > max;
+        if (!result) {
+            return false;
+        }
+
+        int chBytes = "中".getBytes(charset).length;
+        int chMax = max / chBytes;
+
+        //拿到枚举中的message，并替换变量，这个变量是我自己约定的，
+        //约定了两个绑定变量：chMax 和 enMax
+        String message = constraintValidatorContext
+                .getDefaultConstraintMessageTemplate()
+                .replace("{chMax}", String.valueOf(chMax))
+                .replace("{enMax}", String.valueOf(max));
+
+        //禁用默认值，否则会有两条message
+        constraintValidatorContext.disableDefaultConstraintViolation();
+
+        //添加新的message
+        constraintValidatorContext.
+                buildConstraintViolationWithTemplate(message)
+                .addConstraintViolation();
+        return true;
+    }
+}
