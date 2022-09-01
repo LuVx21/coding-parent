d2a3862a7f25017b790dea5441846a43e6e0a720
F.LuVx <yeyu0926@hotmail.com>
2022-09-01 02:59:02 +0000

    update

--- coding-parent-java/coding-root-kotlin/coding-common/src/main/java/org/luvx/common/more/MoreCollections.java
+++ coding-parent-java/coding-root-kotlin/coding-common/src/main/java/org/luvx/common/more/MoreCollections.java
@@ -1,14 +1,25 @@
 package org.luvx.common.more;
 
-import javax.annotation.Nullable;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
+import java.util.SortedSet;
 
 public class MoreCollections {
-    @Nullable
-    public static <T> T first(List<T> es) {
-        if (es == null || es.isEmpty()) {
-            return null;
+    public static <T> Optional<T> first(Collection<T> col) {
+        if (col == null || col.isEmpty()) {
+            return Optional.empty();
         }
-        return es.get(0);
+        T first;
+        if (col instanceof List) {
+            first = ((List<T>) col).get(0);
+        } else if (col instanceof SortedSet) {
+            first = ((SortedSet<T>) col).first();
+        } else {
+            Iterator<T> iterator = col.iterator();
+            first = iterator.hasNext() ? iterator.next() : null;
+        }
+        return Optional.ofNullable(first);
     }
 }
