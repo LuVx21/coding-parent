91117131e19564fab96e7b2a823d9a484f70df61
F.LuVx <yeyu0926@hotmail.com>
2023-10-08 14:49:23 +0800

    update

---
+++ coding-parent-java/coding-root-kotlin/coding-common/src/main/java/org/luvx/coding/common/consts/Common.java
@@ -0,0 +1,8 @@
+package org.luvx.coding.common.consts;
+
+import com.google.common.base.Splitter;
+
+public interface Common {
+    Splitter SPLITTER_COMMA   = Splitter.on(",");
+    Splitter SPLITTER_NEWLINE = Splitter.on("\n");
+}
