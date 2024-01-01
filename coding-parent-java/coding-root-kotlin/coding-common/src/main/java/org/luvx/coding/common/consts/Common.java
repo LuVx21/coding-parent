d225106caaf2373eb6c1ff5666a097e74a38e52a
F.LuVx <yeyu0926@hotmail.com>
2024-01-01 22:27:44 +0800

    update

--- coding-parent-java/coding-root-kotlin/coding-common/src/main/java/org/luvx/coding/common/consts/Common.java
+++ coding-parent-java/coding-root-kotlin/coding-common/src/main/java/org/luvx/coding/common/consts/Common.java
@@ -1,8 +1,14 @@
 package org.luvx.coding.common.consts;
 
+import com.github.phantomthief.util.MoreSuppliers.CloseableSupplier;
 import com.google.common.base.Splitter;
+import com.google.common.util.concurrent.RateLimiter;
+
+import static com.github.phantomthief.util.MoreSuppliers.lazy;
 
 public interface Common {
     Splitter SPLITTER_COMMA   = Splitter.on(",");
     Splitter SPLITTER_NEWLINE = Splitter.on("\n");
+
+    CloseableSupplier<RateLimiter> rateLimiterSupplier = lazy(() -> RateLimiter.create(1));
 }
