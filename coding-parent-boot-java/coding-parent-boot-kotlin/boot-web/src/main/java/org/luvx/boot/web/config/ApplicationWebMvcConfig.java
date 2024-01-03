e812558343ef426bfc8d34a7b9bd8e5d08def6e1
F.LuVx <yeyu0926@hotmail.com>
2024-01-03 21:17:04 +0800

    update

---
+++ coding-parent-boot-java/coding-parent-boot-kotlin/boot-web/src/main/java/org/luvx/boot/web/config/ApplicationWebMvcConfig.java
@@ -0,0 +1,20 @@
+package org.luvx.boot.web.config;
+
+import org.luvx.boot.web.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApplicationWebMvcConfig implements WebMvcConfigurer {
+    @Bean
+    public LogInterceptor logInterceptor() {
+        return new LogInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(logInterceptor());
+    }
+}
