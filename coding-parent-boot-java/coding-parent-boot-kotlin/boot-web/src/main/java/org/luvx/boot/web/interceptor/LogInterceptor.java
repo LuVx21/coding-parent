d76d7294c26b4112c69367ee216b206392ad1ab2
F.LuVx <yeyu0926@hotmail.com>
2024-01-03 21:20:28 +0800

    update

--- coding-parent-boot-java/coding-parent-boot-kotlin/boot-web/src/main/java/org/luvx/boot/web/interceptor/LogInterceptor.java
+++ coding-parent-boot-java/coding-parent-boot-kotlin/boot-web/src/main/java/org/luvx/boot/web/interceptor/LogInterceptor.java
@@ -1,13 +1,13 @@
 package org.luvx.boot.web.interceptor;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.MDC;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Slf4j
