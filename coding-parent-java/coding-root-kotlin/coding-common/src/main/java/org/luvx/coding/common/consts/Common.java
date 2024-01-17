85eca4d6c5e347c2a31a44ebb10de6d2200f8265
F.LuVx <yeyu0926@hotmail.com>
2024-01-17 22:17:33 +0800

    update

--- coding-parent-java/coding-root-kotlin/coding-common/src/main/java/org/luvx/coding/common/consts/Common.java
+++ coding-parent-java/coding-root-kotlin/coding-common/src/main/java/org/luvx/coding/common/consts/Common.java
@@ -3,6 +3,9 @@ package org.luvx.coding.common.consts;
 import com.github.phantomthief.util.MoreSuppliers.CloseableSupplier;
 import com.google.common.base.Splitter;
 import com.google.common.util.concurrent.RateLimiter;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.*;
 
 import static com.github.phantomthief.util.MoreSuppliers.lazy;
 
@@ -11,4 +14,17 @@ public interface Common {
     Splitter SPLITTER_NEWLINE = Splitter.on("\n");
 
     CloseableSupplier<RateLimiter> RATE_LIMITER_SUPPLIER = lazy(() -> RateLimiter.create(1));
+
+    CloseableSupplier<ThreadPoolExecutor> THREAD_POOL_EXECUTOR_SUPPLIER = lazy(() -> {
+        final int corePoolSize = 5;
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder().setNameFormat("common-pool-%d").build();
+        return new ThreadPoolExecutor(corePoolSize,
+                corePoolSize << 1,
+                0L,
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(100),
+                namedThreadFactory,
+                new ThreadPoolExecutor.AbortPolicy()
+        );
+    });
 }
