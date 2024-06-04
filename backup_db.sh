b4a1195d53833879f4dcca17d557f98e01837028
F.LuVx <yeyu0926@hotmail.com>
2024-06-04 18:18:02 +0800

    update

--- backup_db.sh
+++ backup_db.sh
@@ -1,5 +1,9 @@
-cp -r ./.idea/{dataSources.xml,dataSources.local.xml,dataSources} ~/OneDrive/config/jetbrain
+# cp -r ./.idea/{dataSources.xml,dataSources.local.xml,dataSources} ~/OneDrive/config/jetbrain
 # cd ~/Library/Application\ Support/JetBrains/IntelliJIdea2023.3/consoles/
 # cp -r ./db/* ~/OneDrive/config/jetbrain/consoles
 # rm -r ./db
 # ln -s ~/OneDrive/config/jetbrain/consoles db
+
+ln -s ~/OneDrive/config/jetbrain/dataSources dataSources
+ln -s ~/OneDrive/config/jetbrain/dataSources.xml dataSources.xml
+ln -s ~/OneDrive/config/jetbrain/dataSources.local.xml dataSources.local.xml
